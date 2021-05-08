9ab0c082954646336c3145724326edba18b368f0
ktsuc002 <ktsuc002@ucr.edu>
2021-05-08 16:10:49 -0700

    Added puzzle files

---
+++ ktsuc002_customLab_SimonSaysPuzzle/ktsuc002_customLab_SimonSaysPuzzle/main.c
@@ -0,0 +1,18 @@
+/*
+ * ktsuc002_customLab_SimonSaysPuzzle.c
+ *
+ * Created: 5/27/2019 12:11:50 AM
+ * Author : Kris Tsuchiyama
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
